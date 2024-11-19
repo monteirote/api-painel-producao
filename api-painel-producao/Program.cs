5f2c59d680a799724c2732e1536bcaabc8529653
Vinicius Monteiro <vinicius@iconax.com.br>
2024-11-19 13:41:28 -0300

    feat: adicionando configs e packages

--- api-painel-producao/Program.cs
+++ api-painel-producao/Program.cs
@@ -1,6 +1,25 @@
+using api_painel_producao.Repositories;
+using api_painel_producao.Services;
+using api_painel_producao.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("DataSource=app.db; Cache=Shared"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
---
+++ api-painel-producao/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_painel_producao {
+    public static class Settings {
+        public static string Secret = "mudarissoaqui";
+    }
+}
