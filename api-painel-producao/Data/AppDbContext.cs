660e59b8ef44b033e914905947c0bd726a53caf5
Vinicius Monteiro <vinicius@iconax.com.br>
2024-11-22 17:09:21 -0300

    feat: tornando a coluna Username da tabela Users única.

--- api-painel-producao/Data/AppDbContext.cs
+++ api-painel-producao/Data/AppDbContext.cs
@@ -8,5 +8,14 @@ namespace api_painel_producao.Data {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
     }
 }
