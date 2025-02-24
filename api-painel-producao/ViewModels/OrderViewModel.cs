0ec2b2759a34b5114249b174f00823168c4d0894
Vinícius Monteiro <vinicius@iconax.com.br>
2025-02-23 23:18:04 -0300

    feat: continuação refatoração

--- api-painel-producao/ViewModels/OrderViewModel.cs
+++ api-painel-producao/ViewModels/OrderViewModel.cs
@@ -4,28 +4,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace api_painel_producao.ViewModels {
 
-    public class OrderViewModel { }
+    //public class OrderViewModel { }
 
-    public class CreateOrderViewModel {
+    //public class CreateOrderViewModel {
 
-        [Required]
-        public string Reference { get; set; } = string.Empty;
-
-
-        [Required]
-        [EnumValidation(typeof(Priority), ErrorMessage = "Action failed: the value of 'Priority' is not valid.")]
-        public string Priority { get; set; } = string.Empty;
+    //    [Required]
+    //    public string Reference { get; set; } = string.Empty;
 
 
-        [Required]
-        public decimal TotalPrice { get; set; }
+    //    [Required]
+    //    [EnumValidation(typeof(Priority), ErrorMessage = "Action failed: the value of 'Priority' is not valid.")]
+    //    public string Priority { get; set; } = string.Empty;
 
 
-        [Required]
-        public int CustomerId { get; set; }
+    //    [Required]
+    //    public decimal TotalPrice { get; set; }
 
 
-        [Required]
-        public List<CreateFramedArtworkViewModel> FramedArtworks { get; set; } = [];
-    }
+    //    [Required]
+    //    public int CustomerId { get; set; }
+
+
+    //    [Required]
+    //    public List<Framed> FramedArtworks { get; set; } = [];
+    //}
 }
